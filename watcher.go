9edd5c8a63626eb2ab4d2c62f219e7aaf398c552
Parth Mehrotra <parth@mehrotra.me>
2018-08-26 19:55:03 -0400

    initial comit

---
+++ watcher.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/games", Games).Methods("GET")
+	log.Fatal(http.ListenAndServe(":6000", router))
+}
+
+func Games(w http.ResponseWriter, r *http.Request) {
+
+}
